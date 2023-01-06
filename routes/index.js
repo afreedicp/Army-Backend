943c12d7acb9410880c4eef7e421db0b55c9de47
Afreedi <afreedi@hamon.in>
2023-01-06 10:58:10 +0530

    Created first api and updated env

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/Army', (req, res) => {
+router.get('/home', (req, res) => {
   res.status(200).json({
     massage: 'Hello world moine',
   });
